b773c8b1e32e857f431662c0f59d7990ae759f64
kadmy95@gmail.com <kadmy95@gmail.com>
2023-04-06 21:45:33 +0300

    bugs fixed

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App'
+// import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import store from './components/store/store'
 import { Provider } from 'react-redux'
 
 export const root = ReactDOM.createRoot(document.getElementById('root'))
+const App = React.lazy(() => import('./App'))
 
 root.render(
   <React.StrictMode>
