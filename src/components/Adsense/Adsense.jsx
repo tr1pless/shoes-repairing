c69e6fd26309d32db4a10b2d645ae1ec5e9c9598
tr1pless <kadmy95@gmail.com>
2024-12-17 16:06:28 +0200

    adsense upd

--- src/components/Adsense/Adsense.jsx
+++ src/components/Adsense/Adsense.jsx
@@ -3,12 +3,13 @@ import React, { useEffect  } from 'react';
 import {Adsense} from '@ctrl/react-adsense';
 
 export const AdComp = () => {
-    <Adsense
-  client="ca-pub-3179825736250948"
-  slot="5242655036"
-  style={{ display: 'block' }}
-  layout="in-article"
-  format="fluid"
-/>
+    return (    <Adsense
+        client="ca-pub-3179825736250948"
+        slot="5242655036"
+        style={{ display: 'block' }}
+        layout="in-article"
+        format="fluid"
+      />
+      )
 };
 
